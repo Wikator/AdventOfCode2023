e095d5162ccca877eaed0c4a433f7151ce8ebf22
Wikator <wiktor@szymulewicz.com>
2023-12-06 13:50:38 +0100

    Day 5: Removed promises

--- day5/part2.ts
+++ day5/part2.ts
@@ -9,41 +9,27 @@ for (let i: number = 0; i < seeds.length; i += 2) {
     groupedSeeds.push([seeds[i], seeds[i + 1]]);
 }
 
-function seedPromise(groupedSeed: bigint[]): Promise<bigint> {
-    return new Promise((resolve, _): void => {
-        let smallest = null;
-        for (let i: bigint = BigInt(0); i < groupedSeed[1]; i += BigInt(1)) {
-            const newSeed: bigint = seedMaps.reduce((seedMapAcc: bigint, seedMap: SeedMap): bigint => {
-                let newValue: bigint = seedMapAcc;
-                seedMap.maps.every((map: Map): boolean => {
-                    if (newValue > map.sourceRangeStart + map.range - BigInt(1) || newValue < map.sourceRangeStart)
-                        return true;
-
-                    const distance: bigint = seedMapAcc - map.sourceRangeStart;
-                    newValue = map.destinationRangeStart + distance;
-                    return false;
-                }, seedMapAcc);
-                return newValue;
-            }, groupedSeed[0] + i);
-            if (!smallest)
-                smallest = newSeed;
-
-            if (newSeed < smallest) smallest = newSeed;
-        }
-        resolve(smallest);
-    })
-}
-
-const promises: Promise<bigint>[] = [];
+let ans: bigint = null;
 
 groupedSeeds.forEach((groupedSeed: bigint[]): void => {
-    promises.push(seedPromise(groupedSeed));
+    for (let i: bigint = BigInt(0); i < groupedSeed[1]; i += BigInt(1)) {
+
+        const newSeed: bigint = seedMaps.reduce((seedMapAcc: bigint, seedMap: SeedMap): bigint => {
+            let newValue: bigint = seedMapAcc;
+            seedMap.maps.every((map: Map): boolean => {
+
+                if (newValue > map.sourceRangeStart + map.range - BigInt(1) || newValue < map.sourceRangeStart)
+                    return true;
+
+                const distance: bigint =  seedMapAcc - map.sourceRangeStart;
+                newValue = map.destinationRangeStart + distance;
+                return false;
+            }, seedMapAcc);
+            return newValue;
+        }, groupedSeed[0] + i);
+
+        if (!ans || newSeed < ans) ans = newSeed;
+    }
 })
 
-Promise.all(promises).then((values: bigint[]): void => {
-    const smallest: bigint = values.reduce((acc: bigint, curr: bigint): bigint => {
-        if (!acc || acc < curr) return  curr;
-        return acc;
-    }, null);
-    console.log(smallest);
-})
+console.log(ans);
